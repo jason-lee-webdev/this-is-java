aebfc311ae3bdea0af09d8d3bd52961c7eb82e0c
DESKTOP-JF322AK\jason <jason.lee.webdev@gmail.com>
2023-11-23 20:08:19 +0900

    🌈feat : 프로젝트 init

---
+++ src/src/ch12/sec03/exam01/Member.java
@@ -0,0 +1,22 @@
+package src.ch12.sec03.exam01;
+
+public class Member {
+    private String id;
+
+    public Member(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if(object instanceof Member) {
+            Member other = (Member) object;
+
+            if(this.id.equals(other.id)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
